0dd74c8401704566e1bebb31b31bb0f4bd0624a5
Paul Otieno <vitalispaul48@live.com>
2025-11-30 00:52:02 +0300

    Add copy and view functions

--- main.py
+++ main.py
@@ -1,73 +1,34 @@
 import numpy as np
 
+# Copy vs view
 
-list1 = [1, 2, 3, 4, 5]
+# Copy is a copy of your array
+# A view is also a copy but it's still connected to the original array
 
-list2 = ["John Elder", 41, list1, True]
+np1 = np.array([0,1,2,3,4,5,6])
 
+# # Create a view
+# np2 = np1.view()
+#
+# print(f'Original NP1 {np1}') # [0 1 2 3 4 5 6]
+# print(f'Original NP2 {np2}') # [0 1 2 3 4 5 6]
+#
+# np1[0] = 41
+#
+# print(f'Changed NP1 {np1}') # [41  1  2  3  4  5  6]
+# print(f'Original NP2 {np2}') # [41  1  2  3  4  5  6]
 
-# Numpy - Numeric python
-# Data type of a numpy array is ndarray = n-dimensional array
+# If you make a change to the view, the original will also be changed
 
-# Anything in a numpy array has to be of the same data type
+# Create a Copy
 
-np1 = np.array([0, 1, 2, 3, 4, 5])
-print(np1)
+np2 = np1.copy()
 
-# Output: [0 1 2 3 4 5]
+print(f'Original NP1 {np1}') # [0 1 2 3 4 5 6]
+print(f'Original NP2 {np2}') # [0 1 2 3 4 5 6]
+#
+np2[0] = 41
+#
+print(f'Changed NP1 {np1}') # [41  1  2  3  4  5  6]
+print(f'Original NP2 {np2}') # [0  1  2  3  4  5  6]
 
-# Like the len function
-print(np1.shape)
-
-# Output: (6,)
-
-
-# anaother way of creating an array
-np2 = np.arange(10)
-print(np2)
-
-# Output: [0 1 2 3 4 5 6 7 8 9]
-
-# Step
-np3 = np.arange(0, 10, 2)
-print(np3)
-
-# Output: [0 2 4 6 8]
-
-# Zeros
-np4 = np.zeros(10)
-print(np4)
-
-# Output: [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
-
-# Multidimensional zeros
-np5 = np.zeros((2, 10))
-print(np5)
-
-# Output: [[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
-#  [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]]
-
-# Full
-np6 = np.full((10), 6)
-print(np6)
-
-# Output: [6 6 6 6 6 6 6 6 6 6]
-
-# Multidimensional Full
-np7 = np.full((2, 10), 6)
-print(np7)
-
-# Output: [[6 6 6 6 6 6 6 6 6 6]
-#  [6 6 6 6 6 6 6 6 6 6]]
-
-# Convert Python lists to np
-my_list = [1, 2, 3, 4, 5]
-np8 = np.array(my_list)
-
-print(np8)
-
-# Output: [1 2 3 4 5]
-
-print(np8[0])
-
-# Output: 1
