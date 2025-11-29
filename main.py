8269a900531c3bff71edbdf7f12473f240f549aa
Paul Otieno <vitalispaul48@live.com>
2025-11-30 01:05:39 +0300

    Shape and reshape

--- main.py
+++ main.py
@@ -1,73 +1,27 @@
 import numpy as np
 
+# Create 1-D Numpy Array and Get Shape
+np1 = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 
-list1 = [1, 2, 3, 4, 5]
-
-list2 = ["John Elder", 41, list1, True]
-
-
-# Numpy - Numeric python
-# Data type of a numpy array is ndarray = n-dimensional array
-
-# Anything in a numpy array has to be of the same data type
-
-np1 = np.array([0, 1, 2, 3, 4, 5])
-print(np1)
-
-# Output: [0 1 2 3 4 5]
-
-# Like the len function
 print(np1.shape)
 
-# Output: (6,)
+# Create 2-D Array and get Shape, (rows/columns) - 2 rows, 6 columns
 
+np2 = np.array([[1,2,3,4,5,6],[7,8,9,10,11,12]])
 
-# anaother way of creating an array
-np2 = np.arange(10)
-print(np2)
+print(np2.shape) # 2 rows, 6 columns
 
-# Output: [0 1 2 3 4 5 6 7 8 9]
-
-# Step
-np3 = np.arange(0, 10, 2)
+# Reshape 2-D
+np3 = np1.reshape(3,4)
 print(np3)
+print(np3.shape)
 
-# Output: [0 2 4 6 8]
-
-# Zeros
-np4 = np.zeros(10)
+# Reshape 3-D
+np4 = np1.reshape(2, 3, 2)
 print(np4)
+print(np4.shape)
 
-# Output: [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
-
-# Multidimensional zeros
-np5 = np.zeros((2, 10))
+# Flatten to 1-D
+np5 = np4.reshape(-1)
 print(np5)
-
-# Output: [[0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]
-#  [0. 0. 0. 0. 0. 0. 0. 0. 0. 0.]]
-
-# Full
-np6 = np.full((10), 6)
-print(np6)
-
-# Output: [6 6 6 6 6 6 6 6 6 6]
-
-# Multidimensional Full
-np7 = np.full((2, 10), 6)
-print(np7)
-
-# Output: [[6 6 6 6 6 6 6 6 6 6]
-#  [6 6 6 6 6 6 6 6 6 6]]
-
-# Convert Python lists to np
-my_list = [1, 2, 3, 4, 5]
-np8 = np.array(my_list)
-
-print(np8)
-
-# Output: [1 2 3 4 5]
-
-print(np8[0])
-
-# Output: 1
+print(np5.shape)
