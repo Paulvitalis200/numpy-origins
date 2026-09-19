e9cb723f173447d2d21bd595dd8d9088fa26a087
Paul Otieno <vitalispaul48@live.com>
2026-09-19 15:21:01 +0300

    Array stuff

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import numpy as np
 
-array = np.arange(0, 10, 2)
-array2 = np.arange(0, 11, 2, dtype=float)
+array = np.array([200,201,208], dtype=np.int64)
 
-print(array)
-print(array2)
+array_float = np.array([1.0, 2.4, 3.3], dtype=np.float64)
+arr_complex = np.array([1+2j, 3+4j], dtype=np.complex128)
+
+array_bool = np.array([True, False, True], dtype=np.bool_)
+
+array_test = np.array([1, 0, 1, 0], dtype=np.bool_)
+
+
+arr_str = np.array(['a', 'bc', 'dejjhjf'], dtype='S3')
+
+arr_full_str = np.array(['apple', 'box', 'ketchyp'], dtype='str')
+
+arr_obj = np.array([1, 'a', 3.5], dtype=object)
+
+str_array = array.astype(str)
+print(array_float)
+print(arr_complex)
+print(array_bool)
+print(array_test)
+print(arr_str)
+print(arr_full_str)
+
+print(arr_obj)
+print(str_array)
 
 # Output:
-# [0 2 4 6 8]
-# [ 0.  2.  4.  6.  8. 10.]
+# 1.  2.4 3.3]
+# 1.+2.j 3.+4.j]
